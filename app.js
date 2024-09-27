ed4ea5848880d01338370a87577f9843812a347c
DEBAJIT PRAMANICK <120debapramanick@gmail.com>
2024-09-28 00:13:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let show=document.querySelector("#show");
+
+function sinf(){
+    show.value=Math.sin(show.value);
+}
+
+function cosf(){
+    show.value=Math.cos(show.value);
+}
+
+function tanf(){
+    show.value=Math.tan(show.value);
+}
+
+function sqrf(){
+    show.value=Math.sqrt(show.value);
+}
+function powf(){
+    show.value=Math.pow(show.value,2);
+}
